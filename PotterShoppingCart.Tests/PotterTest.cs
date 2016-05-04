81e4e7feccf24c81894186cc92e458afe9f090c1
Chun Wei Tan <chunwei.tan53@titansoft.com.sg>
2016-05-04 22:04:27 +0800

    First commit

---
+++ PotterShoppingCart.Tests/PotterTest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterTest
+    {
+        [TestMethod]
+        public void CalculatePriceTest_
+    }
+}
