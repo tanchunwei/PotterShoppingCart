a21d5353a5a25bbdb43365583aedd718725c44e4
Chun Wei Tan <chunwei.tan53@titansoft.com.sg>
2016-05-04 22:15:33 +0800

    Add Test, one book should cost 100 dollars

--- PotterShoppingCart.Tests/PotterTest.cs
+++ PotterShoppingCart.Tests/PotterTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PotterShoppingCart.Tests
 {
@@ -6,6 +7,17 @@ namespace PotterShoppingCart.Tests
     public class PotterTest
     {
         [TestMethod]
-        public void CalculatePriceTest_
+        public void CalculatePriceTest_PriceOfOneBook_Is100Dollars()
+        {
+            var cart = new List<PotterBook>()
+            {
+                Series = 1
+            };
+            decimal expected = 100;
+            var target = new ShoppingCart(cart);
+            decimal actual = target.calculatePrice();
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
